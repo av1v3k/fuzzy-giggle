e8175c91f939e605a65d372b095cab3d82246009
Vivek <vivek.ananth87@gmail.com>
2023-12-09 18:35:13 +0530

    projects from scratch - learn

--- src/app/state/actions/types/user.enums.ts
+++
@@ -1,5 +0,0 @@
-export enum UserActionTypes {
-    GET_USER_DATA = '[heroes component] get user Data',
-    GET_USER_DATA_SUCCESS = '[heroes component] get user Data Success',
-    GET_USER_DATA_ERROR = '[heroes component] get user Data Error',
-}
--- src/app/state/selectors/user.selector.ts
+++
@@ -1,1 +0,0 @@
-export const getUserResponseSelector = (state: any) => state.user.userres;
